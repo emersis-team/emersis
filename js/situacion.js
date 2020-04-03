252fe097dec0adeb2deab61fc0061cc6761170a6
Paul <sancuba@gmail.com>
2020-04-03 08:59:59 -0300

    BUGFIX Recursos de Facu

--- js/situacion.js
+++ js/situacion.js
@@ -1,3 +1,15 @@
+function agregarEnRecurso() {
+	$('.lineaRelleno:first').clone().appendTo('#listaDeRecursos').attr({});
+	$('#listaDeRecursos:first').find('.lineaRelleno:last')
+			.removeClass("hidden");
+}
+
+function agregarEnNecesidad() {
+	$('.lineaRelleno:first').clone().appendTo('#listaDeNecesidades').attr({});
+	$('#listaDeNecesidades:first').find('.lineaRelleno:last').removeClass(
+			"hidden");
+}
+
 function cargarStuacionDeEntidad() {
 	var user = getCookie("username");
 	if (username != null) {
@@ -6,22 +18,25 @@ function cargarStuacionDeEntidad() {
 				user = entidades[num];
 			}
 		});
-		$('.nombreEntidad').text = user.nombre;
-		var objetoACopiar = $('.lineaRelleno:first');
-		$.each(user.capacidades,
-			function(num) {
-				objetoACopiar.clone().appendTo('.listaDeRecursos');
-				$('.lineaRelleno:last').find('.recurso').text = (user.capacidades[num].recurso.nombre);
-				$('.lineaRelleno:last').find('.cantidad').text = (user.capacidades[num].cantidad);
-				$('.lineaRelleno:last').classList.remove("hidden");
-			});
+		document.getElementById("nombreEntidadRecursos").textContent = user.nombre;
+		$.each(user.capacidades, function(num) {
+			$('.lineaRelleno:last').clone().appendTo('#listaDeRecursos').attr(
+					{});
+			$('.lineaRelleno:last').find('.recurso').val(
+					user.capacidades[num].recurso.nombre);
+			$('.lineaRelleno:last').find('.cantidad').val(
+					user.capacidades[num].cantidad);
+			$('.lineaRelleno:last').removeClass("hidden");
+		});
 
-		$.each(user.necesidades,
-			function(num) {
-				objetoACopiar.clone().appendTo('.listaDeNecesidades');
-				$('.lineaRelleno:last').find('.recurso').text = (user.capacidades[num].recurso.nombre);
-				$('.lineaRelleno:last').find('.cantidad').text = (user.capacidades[num].cantidad);
-				$('.lineaRelleno:last').classList.remove("hidden");
-			});
+		$.each(user.necesidades, function(num) {
+			$('.lineaRelleno:last').clone().appendTo('#listaDeNecesidades')
+					.attr({});
+			$('.lineaRelleno:last').find('.recurso').val(
+					user.necesidades[num].recurso.nombre);
+			$('.lineaRelleno:last').find('.cantidad').val(
+					user.necesidades[num].cantidad);
+			$('.lineaRelleno:last').removeClass("hidden");
+		});
 	}
 };
